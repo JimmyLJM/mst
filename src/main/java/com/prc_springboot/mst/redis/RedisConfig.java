e4246efe65e32adfcd504fe819f5765d363fc4d0
jimmy <ljm506937617@163.com>
2019-11-29 15:49:09 +0800

    redis配置类和工具类

---
+++ src/main/java/com/prc_springboot/mst/redis/RedisConfig.java
@@ -0,0 +1,54 @@
+package com.prc_springboot.mst.redis;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 自定义配置类
+ *
+ * 源码中提供的redisTemplate泛型为[object,object]<br/>
+ * 且注解为 @ConditionalOnMissingBean(name = "redisTemplate")<br/>
+ *
+ * 因此我们自定义配置一个其他更适合使用的redisTemplate
+ */
+@Configuration
+public class RedisConfig {
+
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
--- src/test/java/com/prc_springboot/mst/MstApplicationTests.java
+++ src/test/java/com/prc_springboot/mst/MstApplicationTests.java
@@ -2,15 +2,30 @@ package com.prc_springboot.mst;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MstApplicationTests {
 
+
+	@Resource
+	private RedisTemplate<String, Object> redisTemplate;
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
 	@Test
 	public void contextLoads() {
+		redisTemplate.opsForValue().set("test", 1);
+		int test = (int) redisTemplate.opsForValue().get("test");
+		System.out.println(test);
 	}
 
 }
